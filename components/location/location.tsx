eb26960f4f7538141a9aa8488a15f5839f73f5ff
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-26 18:32:56 +0000

    new folders added

--- components/location/location.tsx
+++ components/location/location.tsx
@@ -36,7 +36,6 @@ export default function Location() {
             width="100%"
             height="100%"
             style={{ border: 0 }}
-            allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>

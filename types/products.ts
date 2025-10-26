16c8361c0cdaf049e1ea82ba4e36f06d20759178
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-26 19:10:37 +0000

    type added

---
+++ types/products.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  image: string;
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}

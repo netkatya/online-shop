517af765bf91d236c82370b9fb79c7845ba8e7b1
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-27 17:18:36 +0000

    modif

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -19,10 +19,8 @@ export default function Pagination({
       nextLabel="→"
       previousLabel="←"
       containerClassName="flex justify-center items-center gap-3 mt-10 select-none"
-      pageClassName="flex items-center justify-center w-10 h-10 rounded-xl border border-gray-300 
-                     bg-gradient-to-b from-white to-gray-50 shadow-md text-gray-700 font-medium 
-                     hover:shadow-lg hover:from-gray-50 hover:to-white 
-                     transition-all duration-200 cursor-pointer active:scale-95"
+      pageClassName="flex items-center justify-center w-10 h-10 rounded-xl
+               "
       activeClassName="!bg-red-500 !border-red-500 shadow-lg"
       renderOnZeroPageCount={null}
     />

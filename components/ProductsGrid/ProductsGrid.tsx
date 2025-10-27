2d4ba42b244bc5b4d6bad31785e399ab1b9f92b4
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-27 16:06:34 +0000

    modif

--- components/ProductsGrid/ProductsGrid.tsx
+++ components/ProductsGrid/ProductsGrid.tsx
@@ -18,41 +18,43 @@ export default function ProductsGrid({ products }: ProductsGridInt) {
           <Image
             src={product.image}
             alt={product.name}
-            className="w-full h-76 object-cover"
+            className="w-full h-70 object-cover"
             height="80"
             width="192"
           ></Image>
-          <div className="p-4">
-            <h2 className="text-lg font-semibold text-gray-800">
+          <div className="p-4 flex flex-col gap-2">
+            <h2 className="text-lg font-semibold text-gray-800 h-[40px]">
               {product.name}
             </h2>
             <p className="text-red-500 font-bold mt-2">${product.price}</p>
-            <p className="text-gray-600 text-sm mt-1">{product.category}</p>
+
             <p className="text-gray-500 text-sm mt-2 line-clamp-2">
               {product.description}
             </p>
-            <div className="flex items-center justify-between">
+            <div className="mt-2 flex items-center justify-between">
               <Link
                 href={`/products/${product._id}`}
-                className="mt-3 inline-block text-red-500 hover:underline"
+                className=" inline-block text-red-500 hover:underline"
               >
                 View Details
               </Link>
 
               <div className="flex items-center gap-2">
                 <button
-                  size="icon"
-                  variant="ghost"
-                  className="flex justify-center items-center border border-red-500 h-[30px] w-[50px] rounded-md hover:bg-gray-100"
+                  className="flex justify-center items-center h-[30px] w-[60px] rounded-lg 
+               border border-red-400 bg-gradient-to-b from-white to-red-50 
+               shadow-md hover:shadow-lg hover:from-red-50 hover:to-white
+               active:scale-95 transition-all duration-200"
                   onClick={() => console.log("Add to favorites:", product._id)}
                 >
                   <Heart className="w-5 h-5 text-red-500" />
                 </button>
 
                 <button
-                  size="icon"
-                  variant="ghost"
-                  className="flex justify-center items-center border border-red-500 h-[30px] w-[50px] rounded-md hover:bg-gray-100"
+                  className="flex justify-center items-center h-[30px] w-[60px] rounded-lg 
+               border border-red-400 bg-gradient-to-b from-white to-red-50 
+               shadow-md hover:shadow-lg hover:from-red-50 hover:to-white
+               active:scale-95 transition-all duration-200"
                   onClick={() => console.log("Add to cart:", product._id)}
                 >
                   <ShoppingCart className="w-5 h-5 text-gray-800" />
